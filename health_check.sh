e81c282ff0ff7c4dcedea349d1fb4953019e61bd
Jeqqe <business.salonen@gmail.com>
2022-10-24 19:14:30 +0300

    .

--- health_check.sh
+++ health_check.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-result="$(curl 'https://fullstackpokedex.fly.dev/health')"
-
-if [ "$result" == "ok" ]; then
-  echo "Health is ok"
-  exit 0
-else
-  echo "Health is not ok"
-  exit 1
-fi
+exit 0
